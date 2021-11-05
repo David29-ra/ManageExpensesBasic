88cc7ed4f149ac9b39f93b150017e59fbaa59be6
David29-ra <davidrivasa.12@gmail.com>
2021-11-05 10:56:32 -0500

    set up for the App with global style and add files like components, pages and services

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { css, Global } from "@emotion/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global
+        styles={css`
+          @import url('https://fonts.googleapis.com/css2?family=Inter&family=Roboto&display=swap');
+          * {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            font-family: 'Inter', sans-serif;
+            box-sizing: border-box;
+            text-decoration: none;
+          }
+          body {
+            background-color: #fafafa;
+          }
+        `}/>
+      <p>Hello World</p>
+      
+    </>
   );
 }
 
